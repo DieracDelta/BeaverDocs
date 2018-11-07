64c0d726996495b08edf9138ebd128f48d759cca
Grace Yin <graceyin1218@gmail.com>
2018-11-07 03:36:48 -0500

    merged with arlene

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const path = require('path');
+
 const peer = require('peer');
 
-let listener;
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/codemirror', express.static(path.join(__dirname, 'public/codemirror')));
 
 app.get('/', function (req, res) {
   // TODO: render something?
-  res.sendFile('views/index.html', {root: __dirname})
+  res.sendFile('index.html', {root: path.join(__dirname, '/public')})
 });
 
 var srv = app.listen(port, function() {
-    console.log('Listening on '+port)
+    console.log('Listening on ' + port)
 })
 
 app.use('/peerjs', peer.ExpressPeerServer(srv, {
